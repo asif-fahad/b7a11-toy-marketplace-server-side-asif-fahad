3e196dcb5d2503d703caf68bf8880f078a54d284
asif-fahad <asif123fahad@gmail.com>
2023-05-19 12:27:36 +0600

    post added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdqyujf.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,6 +24,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toysCollection = client.db('toyDB').collection('toys');
+
+
+        app.post('/toys', async (req, res) => {
+            const newToy = req.body;
+            console.log(newToy);
+            const result = await toysCollection.insertOne(newToy);
+            res.send(result);
+        })
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
