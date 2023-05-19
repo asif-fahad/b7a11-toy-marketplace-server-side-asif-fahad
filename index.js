01c9d9ad3535c4a711f5ae4e05cff156ef927bdc
asif-fahad <asif123fahad@gmail.com>
2023-05-19 17:19:39 +0600

    get by id

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.findOne(query);
+            res.send(result);
+        })
+
 
         app.post('/toys', async (req, res) => {
             const newToy = req.body;
