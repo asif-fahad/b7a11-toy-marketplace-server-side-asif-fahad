c1544cd98f12e5e673a4989c4e959344e3ec7077
asif-fahad <asif123fahad@gmail.com>
2023-05-21 00:51:01 +0600

    vercel config changed to before

--- index.js
+++ index.js
@@ -21,101 +21,104 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    const toysCollection = client.db('toyDB').collection('toys');
-
-    const indexKeys = { name: 1 };
-    const indexOptions = { name: "name" };
-    const result = await toysCollection.createIndex(indexKeys, indexOptions);
-
-
-    app.get("/getToysByText/:text", async (req, res) => {
-        const text = req.params.text;
-        const result = await toysCollection
-            .find({
-                $or: [
-                    { name: { $regex: text, $options: "i" } }
-                ],
-            })
-            .toArray();
-        res.send(result);
-    });
-
-    app.get('/toys', async (req, res) => {
-        const cursor = toysCollection.find().limit(20);
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-
-
-    app.get('/toysEmailSort', async (req, res) => {
-        const sortValue = req.query?.sort;
-        // console.log(sortValue);
-
-        // console.log(req.query.email)
-        let query = {};
-        if (req.query?.email) {
-            query = { email: req.query.email }
-        }
-        else {
-            res.status(404).send({ error: true, message: 'Email not found' })
-        }
-
-        const val = sortValue == 'descending' ? -1 : 1;
-
-        const result = await toysCollection.find(query).sort({ price: val }).toArray();
-        res.send(result);
-    })
-
-    app.get('/toys/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await toysCollection.findOne(query);
-        res.send(result);
-    })
-
-
-    app.post('/toys', async (req, res) => {
-        const newToy = req.body;
-        // console.log(newToy);
-        const result = await toysCollection.insertOne(newToy);
-        res.send(result);
-    })
-
-    app.patch('/toys/:id', async (req, res) => {
-        const id = req.params.id;
-        const filter = { _id: new ObjectId(id) };
-        const options = { upsert: true };
-        const updatedToys = req.body;
-        const toys = {
-            $set: {
-                name: updatedToys.name,
-                sName: updatedToys.sName,
-                email: updatedToys.email,
-                subCategory: updatedToys.subCategory,
-                price: updatedToys.price,
-                rating: updatedToys.rating,
-                quantity: updatedToys.quantity,
-                details: updatedToys.details,
-                photo: updatedToys.photo
-            }
-        }
-
-        const result = await toysCollection.updateOne(filter, toys, options);
-        res.send(result);
-    })
-
-
-    app.delete('/toys/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await toysCollection.deleteOne(query);
-        res.send(result);
-    })
 
 
 
     try {
         // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const toysCollection = client.db('toyDB').collection('toys');
+
+        const indexKeys = { name: 1 };
+        const indexOptions = { name: "name" };
+        const result = await toysCollection.createIndex(indexKeys, indexOptions);
+
+
+        app.get("/getToysByText/:text", async (req, res) => {
+            const text = req.params.text;
+            const result = await toysCollection
+                .find({
+                    $or: [
+                        { name: { $regex: text, $options: "i" } }
+                    ],
+                })
+                .toArray();
+            res.send(result);
+        });
+
+        app.get('/toys', async (req, res) => {
+            const cursor = toysCollection.find().limit(20);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        app.get('/toysEmailSort', async (req, res) => {
+            const sortValue = req.query?.sort;
+            // console.log(sortValue);
+
+            // console.log(req.query.email)
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            else {
+                res.status(404).send({ error: true, message: 'Email not found' })
+            }
+
+            const val = sortValue == 'descending' ? -1 : 1;
+
+            const result = await toysCollection.find(query).sort({ price: val }).toArray();
+            res.send(result);
+        })
+
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.findOne(query);
+            res.send(result);
+        })
+
+
+        app.post('/toys', async (req, res) => {
+            const newToy = req.body;
+            // console.log(newToy);
+            const result = await toysCollection.insertOne(newToy);
+            res.send(result);
+        })
+
+        app.patch('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedToys = req.body;
+            const toys = {
+                $set: {
+                    name: updatedToys.name,
+                    sName: updatedToys.sName,
+                    email: updatedToys.email,
+                    subCategory: updatedToys.subCategory,
+                    price: updatedToys.price,
+                    rating: updatedToys.rating,
+                    quantity: updatedToys.quantity,
+                    details: updatedToys.details,
+                    photo: updatedToys.photo
+                }
+            }
+
+            const result = await toysCollection.updateOne(filter, toys, options);
+            res.send(result);
+        })
+
+
+        app.delete('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
